646c19ac0e224f186c120e3c124bf3b6349693c9
Mauro Santos <mauro.santos@optellum.com>
2021-12-31 17:47:14 +0000

    fix

--- tests.py
+++ tests.py
@@ -476,6 +476,7 @@ def train_model(vgg, model_criterion, model_optimizer, scheduler, num_epochs=10,
 
                 loss_val += loss
                 acc_val += accuracy(preds, labels)
+                labels_cpu = labels.cpu().detach().numpy()
                 prob_cpu = outputs[0].cpu().detach().numpy()
                 prob_cpu = prob_cpu[:, 1]
                 preds_cpu = preds.cpu().detach().numpy()
